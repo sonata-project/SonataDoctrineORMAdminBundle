690ebfbff004c8102f14cb77098381e08bb1d084
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-11-13 23:00:49 +0100

    [Test] make them work on travis-ci.org

---
+++ Tests/tests/vendors.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+
+set_time_limit(0);
+
+
+$vendorDir = __DIR__.'/../../vendor';
+if (!is_dir($vendorDir)) {
+  mkdir($vendorDir);
+}
+
+$deps = array(
+    array('symfony', 'git://github.com/symfony/symfony.git', 'v2.0.5'),
+    array('Sonata/AdminBundle', 'git://github.com/sonata-project/SonataAdminBundle.git', 'master'),
+    array('doctrine', 'git://github.com/doctrine/doctrine2.git', 'master'),
+    array('doctrine-common', 'git://github.com/doctrine/common.git', 'master'),
+    array('knpmenu', 'git://github.com/knplabs/KnpMenu.git', 'master')
+);
+
+foreach ($deps as $dep) {
+    list($name, $url, $rev) = $dep;
+
+    echo "> Installing/Updating $name\n";
+
+    $installDir = $vendorDir.'/'.$name;
+    if (!is_dir($installDir)) {
+        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+    }
+
+    system(sprintf('cd %s && git fetch origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
+}
