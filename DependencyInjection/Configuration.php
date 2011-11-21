2fc435a8e8e68aa97af2ee7e5c8ec17e64e83b80
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-11-21 22:26:48 +0100

    Revert "Merge pull request #13 from hanken/master"
    This reverts commit 3ea84a082ebb9744d476937bf4702d8debf7ceea, reversing
    changes made to c445690945cbf2edb14e2291b4e2544cfc318da9.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -37,7 +37,6 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-             ->scalarNode('model_manager')->defaultValue('doctrine.orm.default_entity_manager')->end()
                 ->arrayNode('templates')
                     ->addDefaultsIfNotSet()
                     ->children()
