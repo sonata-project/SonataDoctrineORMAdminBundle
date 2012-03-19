141d00e00c46692010ba9c7835dbd01e39256947
Alban Seurat <alkpone@alkpone.com>
2012-03-19 13:53:05 +0100

    By default allow to not audit any entities

--- DependencyInjection/Compiler/AddAuditEntityCompilerPass.php
+++ DependencyInjection/Compiler/AddAuditEntityCompilerPass.php
@@ -40,13 +40,12 @@ class AddAuditEntityCompilerPass implements CompilerPassInterface
                 continue;
             }
 
-            if(isset($attributes[0]['audit']) && $attributes[0]['audit'] == 'false') {
-                continue;
-            }
-            
-            $definition = $container->getDefinition($id);
+            if(isset($attributes[0]['audit']) && $attributes[0]['audit'] == 'true') {
+                
+                $definition = $container->getDefinition($id);
 
-            $autitedEntities[] = $this->getModelName($container, $definition->getArgument(1));
+                $autitedEntities[] = $this->getModelName($container, $definition->getArgument(1));
+            }
         }
 
         $autitedEntities = array_unique($autitedEntities);
