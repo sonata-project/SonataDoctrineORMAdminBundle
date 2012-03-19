c7b09924d2e0fa0bc6fa39daf4aa4341c93eb272
Alban Seurat <alkpone@alkpone.com>
2012-03-19 12:17:31 +0100

    Update DependencyInjection/Compiler/AddAuditEntityCompilerPass.php

--- DependencyInjection/Compiler/AddAuditEntityCompilerPass.php
+++ DependencyInjection/Compiler/AddAuditEntityCompilerPass.php
@@ -40,6 +40,10 @@ class AddAuditEntityCompilerPass implements CompilerPassInterface
                 continue;
             }
 
+            if(isset($attributes[0]['audit']) && $attributes[0]['audit'] == 'false') {
+                continue;
+            }
+            
             $definition = $container->getDefinition($id);
 
             $autitedEntities[] = $this->getModelName($container, $definition->getArgument(1));
