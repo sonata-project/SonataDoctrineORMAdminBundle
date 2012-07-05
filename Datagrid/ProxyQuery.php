255c89f2032a52c4ecbb9ed631233c77ee6de9d3
Alban Seurat <alkpone@alkpone.com>
2012-07-05 16:35:10 +0300

    Update master

--- Datagrid/ProxyQuery.php
+++ Datagrid/ProxyQuery.php
@@ -36,13 +36,13 @@ class ProxyQuery implements ProxyQueryInterface
         $queryBuilder = clone $this->queryBuilder;
 
         // todo : check how doctrine behave, potential SQL injection here ...
-        /*if ($this->getSortBy()) {
-            $sortBy = $this->getSortBy();
+        if ($this->getSortBy()) {
+            $sortBy = current(explode(",", $this->getSortBy()));
             if (strpos($sortBy, '.') === false) { // add the current alias
                 $sortBy = $queryBuilder->getRootAlias().'.'.$sortBy;
             }
             $queryBuilder->orderBy($sortBy, $this->getSortOrder());
-        }*/
+        }
 
         return $this->getFixedQueryBuilder($queryBuilder)->getQuery()->execute($params, $hydrationMode);
     }
