3197b01c89a5c8c09d5701a41fdb3453b57b8def
Michel Salib <michelsalib@hotmail.com>
2012-07-13 12:11:59 +0200

    [WIP] Add class filter

---
+++ Filter/ClassFilter.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\DoctrineORMAdminBundle\Filter;
+
+use Sonata\AdminBundle\Form\Type\EqualType;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+
+class ClassFilter extends Filter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function filter(ProxyQueryInterface $queryBuilder, $alias, $field, $data)
+    {
+        if (!$data || !is_array($data) || !array_key_exists('value', $data)) {
+            return;
+        }
+
+        if (strlen($data['value']) == 0) {
+            return;
+        }
+
+        $data['type'] = !isset($data['type']) ?  EqualType::TYPE_IS_EQUAL : $data['type'];
+
+        $operator = $this->getOperator((int) $data['type']);
+
+        if (!$operator) {
+            $operator = 'INSTANCE OF';
+        }
+
+        $this->applyWhere($queryBuilder, sprintf('%s %s %s', $alias, $operator, $data['value']));
+    }
+
+    /**
+     * @param int $type
+     *
+     * @return mixed
+     */
+    private function getOperator($type)
+    {
+        $choices = array(
+            EqualType::TYPE_IS_EQUAL     => 'INSTANCE OF',
+            EqualType::TYPE_IS_NOT_EQUAL => 'NOT INSTANCE OF',
+        );
+
+        return isset($choices[$type]) ? $choices[$type] : false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultOptions()
+    {
+        return array();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFieldType()
+    {
+        return $this->getOption('field_type', 'choice');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFieldOptions()
+    {
+        return $this->getOption('choices', array(
+            'required' => false,
+            'choice_list'  => new ChoiceList(
+                \array_values($this->getOption('sub_classes')),
+                \array_keys($this->getOption('sub_classes'))
+            ),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRenderSettings()
+    {
+        return array('sonata_type_filter_default', array(
+            'operator_type' => 'sonata_type_equal',
+            'field_type'    => $this->getFieldType(),
+            'field_options' => $this->getFieldOptions(),
+            'label'         => $this->getLabel()
+        ));
+    }
+}
