2b06883ea1ed211fceedf03840d2cd317e683134
Sylvain Deloux <sylvain.deloux@fullsix.com>
2013-08-30 14:29:04 +0000

    Added "sortable" option to sort parameters builder

--- Tests/Model/ModelManagerTest.php
+++ Tests/Model/ModelManagerTest.php
@@ -11,15 +11,66 @@
 
 namespace Sonata\DoctrineORMAdminBundle\Tests\Model;
 
+use Sonata\DoctrineORMAdminBundle\Admin\FieldDescription;
 use Sonata\DoctrineORMAdminBundle\Model\ModelManager;
 
 class ModelManagerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFilterEmpty()
+    public function testSortParameters()
     {
         $registry = $this->getMock('Symfony\Bridge\Doctrine\RegistryInterface');
 
-        $manager = new ModelManager($registry);
-    }
+        $manager  = new ModelManager($registry);
 
+        $datagrid1 = $this->getMockBuilder('\Sonata\AdminBundle\Datagrid\Datagrid')->disableOriginalConstructor()->getMock();
+        $datagrid2 = $this->getMockBuilder('\Sonata\AdminBundle\Datagrid\Datagrid')->disableOriginalConstructor()->getMock();
+
+        $field1 = new FieldDescription();
+        $field1->setName('field1');
+
+        $field2 = new FieldDescription();
+        $field2->setName('field2');
+
+        $field3 = new FieldDescription();
+        $field3->setName('field3');
+        $field3->setOption('sortable', 'field3sortBy');
+
+        $datagrid1
+            ->expects($this->any())
+            ->method('getValues')
+            ->will($this->returnValue(array(
+                '_sort_by'    => $field1,
+                '_sort_order' => 'ASC',
+            )));
+
+        $datagrid2
+            ->expects($this->any())
+            ->method('getValues')
+            ->will($this->returnValue(array(
+                '_sort_by'    => $field3,
+                '_sort_order' => 'ASC',
+            )));
+
+        $parameters = $manager->getSortParameters($field1, $datagrid1);
+
+        $this->assertEquals('DESC', $parameters['filter']['_sort_order']);
+        $this->assertEquals('field1', $parameters['filter']['_sort_by']);
+
+        $parameters = $manager->getSortParameters($field2, $datagrid1);
+
+        $this->assertEquals('ASC', $parameters['filter']['_sort_order']);
+        $this->assertEquals('field2', $parameters['filter']['_sort_by']);
+
+        $parameters = $manager->getSortParameters($field3, $datagrid1);
+
+        $this->assertEquals('ASC', $parameters['filter']['_sort_order']);
+        $this->assertEquals('field3sortBy', $parameters['filter']['_sort_by']);
+
+
+
+        $parameters = $manager->getSortParameters($field3, $datagrid2);
+
+        $this->assertEquals('DESC', $parameters['filter']['_sort_order']);
+        $this->assertEquals('field3sortBy', $parameters['filter']['_sort_by']);
+    }
 }
