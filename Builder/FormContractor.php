5b3178b7458d2e0452b1c325ab2219a498859909
Schumacher Raphael <git@raphaelschumacher.ch>
2016-05-28 22:22:09 +0200

    BC solution for deprecated form type feature

--- Builder/FormContractor.php
+++ Builder/FormContractor.php
@@ -76,7 +76,10 @@ class FormContractor implements FormContractorInterface
      */
     public function getFormBuilder($name, array $options = array())
     {
-        return $this->getFormFactory()->createNamedBuilder($name, 'form', null, $options);
+        $formtype = method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix') ?
+                                    'Symfony\Component\Form\Extension\Core\Type\FormType' : 'form';
+
+        return $this->getFormFactory()->createNamedBuilder($name, $formtype, null, $options);
     }
 
     /**
