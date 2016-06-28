c00fa47647b96e54bb87f2f996aaafaabfd2add4
ju1ius <ju1ius@laposte.net>
2016-06-28 15:25:17 +0200

    Add support for FQCN form type referencing to FormContractor
    The getDefaultOptions method didn't check against form types FQCNs,
    causing the default options not to be set.
    See related admin-bundle GitHub issue at
    https://github.com/sonata-project/SonataAdminBundle/issues/3976

--- Tests/Builder/FormContractorTest.php
+++ Tests/Builder/FormContractorTest.php
@@ -11,6 +11,7 @@
 
 namespace Sonata\DoctrineORMAdminBundle\Tests\Builder;
 
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Sonata\DoctrineORMAdminBundle\Builder\FormContractor;
 use Symfony\Component\Form\FormFactoryInterface;
 
@@ -45,5 +46,70 @@ final class FormContractorTest extends \PHPUnit_Framework_TestCase
             'Symfony\Component\Form\FormBuilderInterface',
             $this->formContractor->getFormBuilder('test', array('foo' => 'bar'))
         );
+    }
+
+    public function testDefaultOptionsForSonataFormTypes()
+    {
+        $admin = $this->getMock('Sonata\AdminBundle\Admin\AdminInterface');
+        $modelManager = $this->getMock('Sonata\AdminBundle\Model\ModelManagerInterface');
+        $modelClass = 'FooEntity';
+
+        $admin->method('getModelManager')->willReturn($modelManager);
+        $admin->method('getClass')->willReturn($modelClass);
+
+        $fieldDescription = $this->getMock('Sonata\AdminBundle\Admin\FieldDescriptionInterface');
+        $fieldDescription->method('getAdmin')->willReturn($admin);
+        $fieldDescription->method('getTargetEntity')->willReturn($modelClass);
+        $fieldDescription->method('getAssociationAdmin')->willReturn($admin);
+
+        $modelTypes = array(
+            'sonata_type_model',
+            'sonata_type_model_list',
+            'sonata_type_model_hidden',
+            'sonata_type_model_autocomplete',
+        );
+        $adminTypes = array('sonata_type_admin');
+        $collectionTypes = array('sonata_type_collection');
+        // NEXT_MAJOR: Use only FQCNs when dropping support for Symfony <2.8
+        if (method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix')) {
+            array_push(
+                $modelTypes,
+                'Sonata\AdminBundle\Form\Type\ModelType',
+                'Sonata\AdminBundle\Form\Type\ModelTypeList',
+                'Sonata\AdminBundle\Form\Type\ModelHiddenType',
+                'Sonata\AdminBundle\Form\Type\ModelAutocompleteType'
+            );
+            $adminTypes[] = 'Sonata\AdminBundle\Form\Type\AdminType';
+            $collectionTypes[] = 'Sonata\CoreBundle\Form\Type\CollectionType';
+        }
+
+        // model types
+        foreach ($modelTypes as $formType) {
+            $options = $this->formContractor->getDefaultOptions($formType, $fieldDescription);
+            $this->assertSame($fieldDescription, $options['sonata_field_description']);
+            $this->assertSame($modelClass, $options['class']);
+            $this->assertSame($modelManager, $options['model_manager']);
+        }
+
+        // admin type
+        $fieldDescription->method('getMappingType')->willReturn(ClassMetadata::ONE_TO_ONE);
+        foreach ($adminTypes as $formType) {
+            $options = $this->formContractor->getDefaultOptions($formType, $fieldDescription);
+            $this->assertSame($fieldDescription, $options['sonata_field_description']);
+            $this->assertSame($modelClass, $options['data_class']);
+            $this->assertSame(false, $options['btn_add']);
+            $this->assertSame(false, $options['delete']);
+        }
+
+        // collection type
+        $fieldDescription->method('getMappingType')->willReturn(ClassMetadata::ONE_TO_MANY);
+        foreach ($collectionTypes as $formType) {
+            $options = $this->formContractor->getDefaultOptions($formType, $fieldDescription);
+            $this->assertSame($fieldDescription, $options['sonata_field_description']);
+            $this->assertSame('sonata_type_admin', $options['type']);
+            $this->assertSame(true, $options['modifiable']);
+            $this->assertSame($fieldDescription, $options['type_options']['sonata_field_description']);
+            $this->assertSame($modelClass, $options['type_options']['data_class']);
+        }
     }
 }
