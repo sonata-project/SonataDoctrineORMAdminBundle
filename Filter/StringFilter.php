efa1a123430acedea8ccd0522c81036771029932
Samuel Chiriluta <samuel4x4@users.noreply.github.com>
2016-12-08 11:05:53 +0200

    TYPE_NOT_CONTAINS doesn't consider NULL values
    TYPE_NOT_CONTAINS advanced filter should consider also the NULL values. Instead of
    
    $alias.$field NOT LIKE :$parameterName
    
    you should have
    
    $alias.$field NOT LIKE :$parameterName OR $alias.$field IS NULL
    
    If you have the following values for a field 
    
    ['Susoo', NULL, 'Bibee']
    
    If you filter by << does not contain 'Susoo' >>, it should return [NULL, 'Bibee'], not only ['Bibee'] as it is implemented right now.

--- Filter/StringFilter.php
+++ Filter/StringFilter.php
@@ -41,8 +41,15 @@ class StringFilter extends Filter
 
         // c.name > '1' => c.name OPERATOR :FIELDNAME
         $parameterName = $this->getNewParameterName($queryBuilder);
-        $this->applyWhere($queryBuilder, sprintf('%s.%s %s :%s', $alias, $field, $operator, $parameterName));
 
+        $whereCondition = sprintf('%s.%s %s :%s', $alias, $field, $operator, $parameterName);
+
+        if ($data['type'] == ChoiceType::TYPE_NOT_CONTAINS) {
+            $whereCondition .= sprintf(' OR %s.%s IS NULL', $alias, $field);
+        }
+        
+        $this->applyWhere($queryBuilder, $whereCondition);
+        
         if ($data['type'] == ChoiceType::TYPE_EQUAL) {
             $queryBuilder->setParameter($parameterName, $data['value']);
         } else {
