627c8225c6ee9249673b742dff20cea96771217b
Oskar Stark <oskarstark@googlemail.com>
2017-06-28 11:02:51 +0200

    CS fix

--- Tests/Guesser/FilterTypeGuesserTest.php
+++ Tests/Guesser/FilterTypeGuesserTest.php
@@ -1,13 +1,23 @@
 <?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Sonata\DoctrineORMAdminBundle\Tests\Guesser;
 
-use PHPUnit\Framework\TestCase;
 use Sonata\DoctrineORMAdminBundle\Guesser\FilterTypeGuesser;
 
-class FilterTypeGuesserTest extends TestCase
+class FilterTypeGuesserTest extends \PHPUnit_Framework_TestCase
 {
     private $guesser;
     private $modelManager;
+    private $metadata;
 
     protected function setUp()
     {
@@ -23,11 +33,11 @@ class FilterTypeGuesserTest extends TestCase
     {
         $class = 'My\Model';
         $property = 'whatever';
-        $this->modelManager->getParentMetadataForProperty($class, $property)->willReturn([
+        $this->modelManager->getParentMetadataForProperty($class, $property)->willReturn(array(
             $this->metadata->reveal(),
             $property,
-            'parent mappings, no idea what it looks like'
-        ]);
+            'parent mappings, no idea what it looks like',
+        ));
         $this->guesser->guessType($class, $property, $this->modelManager->reveal());
     }
 }
