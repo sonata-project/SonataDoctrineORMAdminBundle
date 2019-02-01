5f3d67a1993bbfc14f170e13ea41f996426cee74
Javier Spagnoletti <phansys@gmail.com>
2019-01-31 23:26:29 -0300

    Remove call to `call_user_func_array()` at `ProxyQuery::__call()`

--- src/Datagrid/ProxyQuery.php
+++ src/Datagrid/ProxyQuery.php
@@ -75,7 +75,7 @@ class ProxyQuery implements ProxyQueryInterface
 
     public function __call($name, $args)
     {
-        return \call_user_func_array([$this->queryBuilder, $name], $args);
+        return $this->queryBuilder->$name(...$args);
     }
 
     public function __get($name)
