a02708420a9a1778c9ea0cd5c5e1e210704782b8
Peter Gribanov <wcode404@gmail.com>
2019-08-13 15:13:00 +0300

    test convert data type to string in ModelManager::getValueFromType()

---
+++ tests/Fixtures/Entity/ProductId.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Sonata\DoctrineORMAdminBundle\Tests\Fixtures\Entity;
+
+final class ProductId
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @param int $id
+     */
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
