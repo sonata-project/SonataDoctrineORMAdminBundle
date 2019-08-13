34ace1b4a266a0c86d255ec4e3a773985eaba45d
Peter Gribanov <wcode404@gmail.com>
2019-08-13 16:55:27 +0300

    remove docblocks

--- tests/Fixtures/Entity/Product.php
+++ tests/Fixtures/Entity/Product.php
@@ -14,37 +14,21 @@ namespace Sonata\DoctrineORMAdminBundle\Tests\Fixtures\Entity;
 
 class Product
 {
-    /**
-     * @var ProductId
-     */
     private $id;
 
-    /**
-     * @var string
-     */
     private $name;
 
-    /**
-     * @param ProductId $id
-     * @param string    $name
-     */
     public function __construct(ProductId $id, string $name)
     {
         $this->id = $id;
         $this->name = $name;
     }
 
-    /**
-     * @return ProductId
-     */
     public function getId(): ProductId
     {
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
--- tests/Fixtures/Entity/ProductId.php
+++ tests/Fixtures/Entity/ProductId.php
@@ -14,22 +14,13 @@ namespace Sonata\DoctrineORMAdminBundle\Tests\Fixtures\Entity;
 
 final class ProductId
 {
-    /**
-     * @var int
-     */
     private $id;
 
-    /**
-     * @param int $id
-     */
     public function __construct(int $id)
     {
         $this->id = $id;
     }
 
-    /**
-     * @return int
-     */
     public function getId(): int
     {
         return $this->id;
