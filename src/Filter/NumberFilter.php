20dd45a7607344ccb01a9895cc6e685b2a77494c
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-02-24 11:58:48 +0100

    3.30

--- src/Filter/NumberFilter.php
+++ src/Filter/NumberFilter.php
@@ -53,7 +53,7 @@ class NumberFilter extends Filter
         // NEXT_MAJOR: Remove this if and the (int) cast.
         if (!\is_int($type)) {
             @trigger_error(
-                'Passing a non integer type is deprecated since sonata-project/doctrine-orm-admin-bundle 3.x'
+                'Passing a non integer type is deprecated since sonata-project/doctrine-orm-admin-bundle 3.30'
                 .' and will throw a \TypeError error in version 4.0.',
             );
         }
@@ -86,7 +86,7 @@ class NumberFilter extends Filter
         if (!isset(self::CHOICES[$type])) {
             // NEXT_MAJOR: Throw an \OutOfRangeException instead.
             @trigger_error(
-                'Passing a non supported type is deprecated since sonata-project/doctrine-orm-admin-bundle 3.x'
+                'Passing a non supported type is deprecated since sonata-project/doctrine-orm-admin-bundle 3.30'
                 .' and will throw an \OutOfRangeException error in version 4.0.',
             );
 //            throw new \OutOfRangeException(sprintf(
