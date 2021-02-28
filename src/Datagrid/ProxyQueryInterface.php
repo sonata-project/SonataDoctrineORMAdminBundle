66aa2c4c97d44840b3f04aeaf80c936cc70bfad4
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-02-28 14:38:00 +0100

    Add `ProxyQueryInterface::getDoctrineQuery()`

--- src/Datagrid/ProxyQueryInterface.php
+++ src/Datagrid/ProxyQueryInterface.php
@@ -13,13 +13,28 @@ declare(strict_types=1);
 
 namespace Sonata\DoctrineORMAdminBundle\Datagrid;
 
+use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
 use Sonata\AdminBundle\Datagrid\ProxyQueryInterface as BaseProxyQueryInterface;
 
+/**
+ * NEXT_MAJOR: Remove the "@method" and uncomment the methods instead.
+ *
+ * @method Query getDoctrineQuery()
+ */
 interface ProxyQueryInterface extends BaseProxyQueryInterface
 {
     /**
      * @return QueryBuilder
      */
     public function getQueryBuilder();
+
+//    /**
+//     * This method should be preferred over `$this->getQueryBuilder()->getQuery()`
+//     * since some changed are done to the query builder in order to handle all the
+//     * previously called Sonata\AdminBundle\Datagrid\ProxyQueryInterface methods.
+//     *
+//     * @return Query
+//     */
+//    public function getDoctrineQuery(): Query;
 }
