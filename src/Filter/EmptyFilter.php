af78eff62a534e5c229dba0bbfe9c333e6f6520e
David Maicher <mail@dmaicher.de>
2021-03-11 20:39:47 +0100

    review
    Co-authored-by: Wojciech Błoszyk <wbloszyk@gmail.com>

--- src/Filter/EmptyFilter.php
+++ src/Filter/EmptyFilter.php
@@ -24,7 +24,7 @@ final class EmptyFilter extends NullFilter
     {
         // NEXT_MAJOR: remove this file
         @trigger_error(sprintf(
-            'The %s class is deprecated since sonata-project/doctrine-orm-admin-bundle version 3.27 and will be removed in 4.0.'
+            'The "%s" class is deprecated since sonata-project/doctrine-orm-admin-bundle version 3.27 and will be removed in 4.0.'
             .' Use %s instead.',
             __CLASS__,
             NullFilter::class
