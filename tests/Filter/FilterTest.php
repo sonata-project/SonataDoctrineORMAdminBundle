051f28515c2633463182ca8cba3b69733cc24b33
Javier Spagnoletti <phansys@gmail.com>
2021-03-20 10:34:41 -0300

    Fix filter query when using "OR" conditions

--- tests/Filter/FilterTest.php
+++ tests/Filter/FilterTest.php
@@ -13,11 +13,14 @@ declare(strict_types=1);
 
 namespace Sonata\DoctrineORMAdminBundle\Tests\Filter;
 
-use PHPUnit\Framework\TestCase;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr;
 use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery;
 use Sonata\DoctrineORMAdminBundle\Filter\Filter;
+use Sonata\DoctrineORMAdminBundle\Filter\StringFilter;
 
-final class FilterTest extends TestCase
+final class FilterTest extends FilterTestCase
 {
     /**
      * @var Filter
@@ -56,6 +59,70 @@ final class FilterTest extends TestCase
     public function testIsActive(): void
     {
         $this->assertFalse($this->filter->isActive());
+    }
+
+    /**
+     * @dataProvider orExpressionProvider
+     */
+    public function testOrExpression(string $expected, array $filterOptions = []): void
+    {
+        $entityManager = $this->createStub(EntityManagerInterface::class);
+        $entityManager->method('getExpressionBuilder')->willReturn(new Expr());
+        $queryBuilder = new TestQueryBuilder($entityManager);
+
+        $queryBuilder->select('e')->from('MyEntity', 'e');
+
+        // Some custom conditions set previous to the filters.
+        $queryBuilder
+            ->where($queryBuilder->expr()->eq(1, 2))
+            ->andWhere(
+                $queryBuilder->expr()->orX(
+                    $queryBuilder->expr()->eq(':parameter_1', 4),
+                    $queryBuilder->expr()->eq(5, 6)
+                )
+            )
+            ->setParameter('parameter_1', 3);
+
+        $proxyQuery = new ProxyQuery($queryBuilder);
+        $this->assertSameQuery([], $proxyQuery);
+
+        $filter1 = new StringFilter();
+        $filter1->setCondition(Filter::CONDITION_OR);
+        $filter1->initialize('field_name', $filterOptions);
+
+        $filter1->filter($proxyQuery, 'e', 'project', ['value' => 'sonata-project']);
+
+        $filter2 = new StringFilter();
+        $filter2->setCondition(Filter::CONDITION_OR);
+        $filter2->initialize('field_name', $filterOptions);
+
+        $filter2->filter($proxyQuery, 'e', 'version', ['value' => '3.x']);
+
+        // More custom conditions set after the filters.
+        $queryBuilder->andWhere($queryBuilder->expr()->eq(7, 8));
+
+        $this->assertSame($expected, $queryBuilder->getDQL());
+    }
+
+    public function orExpressionProvider(): iterable
+    {
+        yield 'Using "admin_code" option' => [
+            'SELECT e FROM MyEntity e WHERE 1 = 2 AND (:parameter_1 = 4 OR 5 = 6)'
+            .' AND (e.project LIKE :field_name_0 OR e.version LIKE :field_name_1) AND 7 = 8',
+            [
+                'allow_empty' => false,
+                'admin_code' => 'my_admin',
+            ],
+        ];
+
+        yield 'Missing "admin_code" option, fallback to DQL marker' => [
+            'SELECT e FROM MyEntity e WHERE 1 = 2 AND (:parameter_1 = 4 OR 5 = 6)'
+            .' AND (:sonata_admin_datagrid_filter_query_marker IS NULL'
+            .' OR e.project LIKE :field_name_0 OR e.version LIKE :field_name_1) AND 7 = 8',
+            [
+                'allow_empty' => false,
+            ],
+        ];
     }
 
     private function createFilter(): Filter
