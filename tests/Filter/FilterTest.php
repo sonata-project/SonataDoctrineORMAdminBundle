4f672afcfe7dee1485fe330d8137afaf522631cf
Javier Spagnoletti <phansys@gmail.com>
2021-03-21 21:45:47 -0300

    Update `FilterTest`

--- tests/Filter/FilterTest.php
+++ tests/Filter/FilterTest.php
@@ -17,69 +17,78 @@ use PHPUnit\Framework\TestCase;
 use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
 use Sonata\DoctrineORMAdminBundle\Filter\Filter;
 
-class FilterTest_Filter extends Filter
+final class FilterTest extends TestCase
 {
     /**
-     * Apply the filter to the QueryBuilder instance.
-     *
-     * @param $queryBuilder
-     * @param string $alias
-     * @param string $field
-     * @param string $value
+     * @var Filter
      */
-    public function filter(ProxyQueryInterface $queryBuilder, $alias, $field, $value): void
+    private $filter;
+
+    protected function setUp(): void
     {
-        // TODO: Implement filter() method.
+        parent::setUp();
+
+        $this->filter = $this->createFilter();
     }
 
-    public function getDefaultOptions()
-    {
-        return ['option1' => 2];
-    }
-
-    public function getRenderSettings()
-    {
-        return ['sonata_type_filter_default', [
-            'type' => $this->getFieldType(),
-            'options' => $this->getFieldOptions(),
-        ]];
-    }
-
-    public function testAssociation(ProxyQueryInterface $queryBuilder, $value)
-    {
-        return $this->association($queryBuilder, $value);
-    }
-}
-
-class FilterTest extends TestCase
-{
     public function testFieldDescription(): void
     {
-        $filter = new FilterTest_Filter();
-        $this->assertSame(['option1' => 2], $filter->getDefaultOptions());
-        $this->assertNull($filter->getOption('1'));
+        $this->assertSame(['option1' => 2], $this->filter->getDefaultOptions());
+        $this->assertNull($this->filter->getOption('1'));
 
-        $filter->initialize('field_name', ['field_options' => ['class' => 'FooBar']]);
+        $this->filter->initialize('field_name', ['field_options' => ['class' => 'FooBar']]);
 
-        $this->assertSame(2, $filter->getOption('option1'));
-        $this->assertNull($filter->getOption('foo'));
-        $this->assertSame('bar', $filter->getOption('foo', 'bar'));
+        $this->assertSame(2, $this->filter->getOption('option1'));
+        $this->assertNull($this->filter->getOption('foo'));
+        $this->assertSame('bar', $this->filter->getOption('foo', 'bar'));
 
-        $this->assertSame('field_name', $filter->getName());
-        $this->assertSame(['class' => 'FooBar'], $filter->getFieldOptions());
+        $this->assertSame('field_name', $this->filter->getName());
+        $this->assertSame(['class' => 'FooBar'], $this->filter->getFieldOptions());
     }
 
     public function testExceptionOnEmptyFieldName(): void
     {
         $this->expectException(\RuntimeException::class);
 
-        $filter = new FilterTest_Filter();
-        $filter->getFieldName();
+        $this->filter->getFieldName();
     }
 
     public function testIsActive(): void
     {
-        $filter = new FilterTest_Filter();
-        $this->assertFalse($filter->isActive());
+        $this->assertFalse($this->filter->isActive());
+    }
+
+    private function createFilter(): Filter
+    {
+        return new class() extends Filter {
+            /**
+             * Applies the filter to the QueryBuilder instance.
+             *
+             * @param string $alias
+             * @param string $field
+             * @param string $value
+             */
+            public function filter(ProxyQueryInterface $queryBuilder, $alias, $field, $value): void
+            {
+                // TODO: Implement filter() method.
+                throw new \BadMethodCallException(sprintf(
+                    'Method "%s()" is not implemented.',
+                    __METHOD__
+                ));
+            }
+
+            public function getDefaultOptions(): array
+            {
+                return ['option1' => 2];
+            }
+
+            public function getRenderSettings(): array
+            {
+                return ['sonata_type_filter_default', [
+                    'type' => $this->getFieldType(),
+                    'options' => $this->getFieldOptions(),
+                ]];
+            }
+        };
     }
 }
