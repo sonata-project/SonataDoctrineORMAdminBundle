9b8548715b1de6c9de2c52cf238c9072de328a65
Fran Moreno <franmomu@gmail.com>
2021-05-02 18:45:13 +0200

    Test model autocomplete

---
+++ tests/App/Admin/BookWithAuthorAutocompleteAdmin.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\DoctrineORMAdminBundle\Tests\App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+
+/**
+ * @phpstan-extends AbstractAdmin<\Sonata\DoctrineORMAdminBundle\Tests\App\Entity\Book>
+ */
+final class BookWithAuthorAutocompleteAdmin extends AbstractAdmin
+{
+    protected $baseRoutePattern = 'book-with-author-autocomplete';
+    protected $baseRouteName = 'book_with_author_autocomplete';
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('author', ModelAutocompleteType::class, [
+                'required' => false,
+                'property' => ['name'],
+                'minimum_input_length' => 0,
+            ]);
+    }
+}
