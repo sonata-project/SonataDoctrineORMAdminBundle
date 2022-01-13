2b95684e6368b2dcd3f5efb235f89626455fc72b
Fran Moreno <franmomu@gmail.com>
2022-01-13 19:51:02 +0100

    Fix detection of use of output walkers in pagination (#1616)

---
+++ tests/App/Entity/ProductAttribute.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\DoctrineORMAdminBundle\Tests\App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Entity */
+class ProductAttribute
+{
+    /**
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Product")
+     *
+     * @var Product
+     */
+    private $product;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $name;
+
+    public function __construct(Product $product, string $name)
+    {
+        $this->product = $product;
+        $this->name = $name;
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+}
