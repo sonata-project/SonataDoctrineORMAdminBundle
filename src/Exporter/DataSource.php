c81b9405a7fabc90e185722034232db5ce10b7e3
Willem Verspyck <willemverspyck@users.noreply.github.com>
2022-02-28 16:25:20 +0100

    Fix problem when sorting on ManyToOne column

--- src/Exporter/DataSource.php
+++ src/Exporter/DataSource.php
@@ -34,10 +34,23 @@ final class DataSource implements DataSourceInterface
             throw new \TypeError(sprintf('The query MUST implement %s.', ProxyQueryInterface::class));
         }
 
+        $rootAlias = current($query->getQueryBuilder()->getRootAliases());
+
         // Distinct is needed to iterate, even if group by is used
         // @see https://github.com/doctrine/orm/issues/5868
         $query->getQueryBuilder()->distinct();
-        $query->getQueryBuilder()->select(current($query->getQueryBuilder()->getRootAliases()));
+        $query->getQueryBuilder()->select($rootAlias);
+
+        $sortBy = $query->getSortBy();
+
+        if (null !== $sortBy) {
+            $rootAliasSortBy = strstr($sortBy, '.', true);
+            
+            if ($rootAliasSortBy !== $rootAlias) {
+                $query->getQueryBuilder()->addSelect($rootAliasSortBy);
+            }
+        }
+
         $query->setFirstResult(null);
         $query->setMaxResults(null);
 
