3c806634cc704e1082c684b1a9fbfdf2e77a273f
Willem Verspyck <willemverspyck@users.noreply.github.com>
2022-03-01 17:04:32 +0100

    Export not working when sorting on ManyToOne column (#1639)
    * Remove useless "mapping_type" option
    
    * Remove "mapping_type" field
    
    * Fix tests
    
    * Fix tests
    
    * Options "mapping_type" not needed anymore
    
    * Fix tests
    
    * Fix problem when sorting on ManyToOne column
    
    * Lint fix
    
    * Add test and comment

--- src/Exporter/DataSource.php
+++ src/Exporter/DataSource.php
@@ -34,10 +34,25 @@ final class DataSource implements DataSourceInterface
             throw new \TypeError(sprintf('The query MUST implement %s.', ProxyQueryInterface::class));
         }
 
+        $rootAlias = current($query->getQueryBuilder()->getRootAliases());
+
         // Distinct is needed to iterate, even if group by is used
         // @see https://github.com/doctrine/orm/issues/5868
         $query->getQueryBuilder()->distinct();
-        $query->getQueryBuilder()->select(current($query->getQueryBuilder()->getRootAliases()));
+        $query->getQueryBuilder()->select($rootAlias);
+
+        $sortBy = $query->getSortBy();
+
+        // AddSelect is needed when exporting the results sorted by a column that is part of ManyToOne relation
+        // @see https://github.com/sonata-project/SonataDoctrineORMAdminBundle/issues/1586
+        if (null !== $sortBy) {
+            $rootAliasSortBy = strstr($sortBy, '.', true);
+
+            if ($rootAliasSortBy !== $rootAlias) {
+                $query->getQueryBuilder()->addSelect($rootAliasSortBy);
+            }
+        }
+
         $query->setFirstResult(null);
         $query->setMaxResults(null);
 
