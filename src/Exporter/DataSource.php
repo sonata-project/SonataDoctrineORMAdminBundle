3d158219e1b03d04c4395b7841889085d192f799
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2022-08-02 18:10:57 +0200

    Add support for exporter 3 (#1680)
    * Add support for exporter 3
    
    * Update composer.json

--- src/Exporter/DataSource.php
+++ src/Exporter/DataSource.php
@@ -17,18 +17,10 @@ use Sonata\AdminBundle\Datagrid\ProxyQueryInterface as BaseProxyQueryInterface;
 use Sonata\AdminBundle\Exporter\DataSourceInterface;
 use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQueryInterface;
 use Sonata\Exporter\Source\DoctrineORMQuerySourceIterator;
-use Sonata\Exporter\Source\SourceIteratorInterface;
 
 final class DataSource implements DataSourceInterface
 {
-    /**
-     * NEXT_MAJOR: Return \Iterator instead.
-     *
-     * @psalm-suppress DeprecatedClass
-     *
-     * @see https://github.com/sonata-project/exporter/pull/532
-     */
-    public function createIterator(BaseProxyQueryInterface $query, array $fields): SourceIteratorInterface
+    public function createIterator(BaseProxyQueryInterface $query, array $fields): \Iterator
     {
         if (!$query instanceof ProxyQueryInterface) {
             throw new \TypeError(sprintf('The query MUST implement %s.', ProxyQueryInterface::class));
