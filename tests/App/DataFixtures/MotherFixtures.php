3dbc12352dd0687c3a527c056f5fb9a71fff655f
Jordi <jordism91@gmail.com>
2023-04-10 13:15:22 +0200

    Add tests for collection removal without triggering validation

---
+++ tests/App/DataFixtures/MotherFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\DoctrineORMAdminBundle\Tests\App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Sonata\DoctrineORMAdminBundle\Tests\App\Entity\Child;
+use Sonata\DoctrineORMAdminBundle\Tests\App\Entity\Mother;
+
+final class MotherFixtures extends Fixture implements FixtureInterface
+{
+    public const MOTHER = 'mother';
+
+    public function load(ObjectManager $manager): void
+    {
+        $mother = new Mother();
+        $this->addChildren($mother, 1);
+
+        $manager->persist($mother);
+        $manager->flush();
+
+        $this->addReference(self::MOTHER, $mother);
+    }
+
+    private function addChildren(Mother $mother, int $children): void
+    {
+        for ($i = 0; $i < $children; ++$i) {
+            $child = new Child();
+            $child->setName('Child '.$i);
+            $child->setAnotherName('Another Name '.$i);
+
+            $mother->addChild($child);
+        }
+    }
+}
---
+++ tests/Functional/CollectionItemRemovalTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\DoctrineORMAdminBundle\Tests\Functional;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Panther\DomCrawler\Crawler;
+
+final class CollectionItemRemovalTest extends BasePantherTestCase
+{
+    public function testRemoveCollectionItemWithoutValidation(): void
+    {
+        $crawler = $this->client->request(Request::METHOD_GET, '/admin/tests/app/mother/1/edit?uniqid=mother');
+
+        $form = $crawler->selectButton('Update')->form();
+        $form['mother[children][0][name]'] = '';
+
+        $crawler->filter('.icheckbox_square-blue')->each(static function (Crawler $label): void {
+            $label->click();
+        });
+
+        $this->client->submit($form);
+
+        self::assertSelectorTextContains('.alert-success', 'Item "1" has been successfully updated.');
+    }
+}
