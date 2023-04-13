808efa1e5e19bec9e5b32073ffa151c452378752
Jordi Sala Morales <jordism91@gmail.com>
2023-04-13 16:40:56 +0000

    Recover removed test

--- tests/Exporter/DataSourceTest.php
+++ tests/Exporter/DataSourceTest.php
@@ -58,4 +58,37 @@ final class DataSourceTest extends TestCase
         $iterator = $this->dataSource->createIterator($proxyQuery, []);
         static::assertInstanceOf(DoctrineORMQuerySourceIterator::class, $iterator);
     }
+
+    public function testCreateIteratorWithNormalSortBy(): void
+    {
+        $configuration = $this->createStub(Configuration::class);
+        $configuration->method('getDefaultQueryHints')->willReturn([]);
+
+        $em = $this->createStub(EntityManager::class);
+        $em->method('getConfiguration')->willReturn($configuration);
+
+        $queryBuilder = $this->getMockBuilder(QueryBuilder::class)
+            ->setConstructorArgs([$em])
+            ->getMock();
+
+        $query = new Query($em);
+
+        $queryBuilder->expects(static::once())->method('getDQLPart')->willReturn([]);
+        $queryBuilder->expects(static::once())->method('getRootAliases')->willReturn(['o', 'a', 'e']);
+        $queryBuilder->expects(static::once())->method('distinct');
+        $queryBuilder->expects(static::once())->method('select')->with('o');
+        $queryBuilder->expects(static::once())->method('addSelect')->with('a');
+
+        $proxyQuery = $this->createMock(ProxyQueryInterface::class);
+        $proxyQuery->method('getSortBy')->willReturn('a.column');
+
+        $proxyQuery->method('getQueryBuilder')->willReturn($queryBuilder);
+        $proxyQuery->method('getDoctrineQuery')->willReturn($query);
+        $proxyQuery->expects(static::once())->method('setFirstResult')->with(null);
+        $proxyQuery->expects(static::once())->method('setMaxResults')->with(null);
+
+        $iterator = $this->dataSource->createIterator($proxyQuery, []);
+
+        static::assertInstanceOf(DoctrineORMQuerySourceIterator::class, $iterator);
+    }
 }
