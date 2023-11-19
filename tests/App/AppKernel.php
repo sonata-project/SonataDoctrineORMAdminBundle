6fbc61a652a2964f314ceff9a44780791ba0f914
David Maicher <mail@dmaicher.de>
2023-11-19 19:17:20 +0100

    Use block-bundle ^5.0

--- tests/App/AppKernel.php
+++ tests/App/AppKernel.php
@@ -79,9 +79,6 @@ final class AppKernel extends Kernel
         $routes->import(sprintf('%s/config/routes.yaml', $this->getProjectDir()));
     }
 
-    /**
-     * @psalm-suppress DeprecatedClass
-     */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->setParameter('app.base_dir', $this->getBaseDir());
