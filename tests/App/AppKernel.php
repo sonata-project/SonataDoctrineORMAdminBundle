8cc97bb57c0a573ec622aff20336139eba0eca16
David Maicher <mail@dmaicher.de>
2025-10-12 18:39:56 +0200

    Add support for `doctrine/doctrine-bundle` 3.0

--- tests/App/AppKernel.php
+++ tests/App/AppKernel.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Sonata\DoctrineORMAdminBundle\Tests\App;
 
 use DAMA\DoctrineTestBundle\DAMADoctrineTestBundle;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\CacheCompatibilityPass;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
 use Knp\Bundle\MenuBundle\KnpMenuBundle;
@@ -89,6 +90,28 @@ final class AppKernel extends Kernel
         $container->setParameter('app.base_dir', $this->getBaseDir());
 
         $loader->load(__DIR__.'/config/config.yml');
+
+        if (\PHP_VERSION_ID >= 80400) {
+            $container->loadFromExtension('doctrine', [
+                'orm' => [
+                    'enable_native_lazy_objects' => true,
+                ],
+            ]);
+        }
+
+        if (class_exists(CacheCompatibilityPass::class)) {
+            // doctrine-bundle v2
+            $container->loadFromExtension('doctrine', [
+                'dbal' => [
+                    'use_savepoints' => true,
+                ],
+                'orm' => [
+                    'auto_generate_proxy_classes' => true,
+                    'report_fields_where_declared' => true,
+                ],
+            ]);
+        }
+
         $loader->load(__DIR__.'/config/services.php');
     }
 
